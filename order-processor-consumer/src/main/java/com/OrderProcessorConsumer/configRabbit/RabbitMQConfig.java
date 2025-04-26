f42fb7bc7f4626d6a42293bb71e2c30a8cb241f4
MarvinAchas <marvinacha@ghlperu.com>
2025-04-25 23:25:43 -0500

    fix: configures the consumer to have the queue already created

--- order-processor-consumer/src/main/java/com/OrderProcessorConsumer/configRabbit/RabbitMQConfig.java
+++ order-processor-consumer/src/main/java/com/OrderProcessorConsumer/configRabbit/RabbitMQConfig.java
@@ -1,11 +1,42 @@
 package com.OrderProcessorConsumer.configRabbit;
 
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitMQConfig {
+
+    @Value("${rabbitmq.queue.orders.name}")
+    private String ordersQueueName;
+
+    @Value("${rabbitmq.exchange.name}")
+    private String exchangeName;
+
+    @Value("${rabbitmq.routing.orders.key}")
+    private String routingOrdersKey;
+
+    @Bean
+    public Queue ordersQueue(){
+        return new Queue(ordersQueueName, true);
+    }
+
+    @Bean
+    public TopicExchange ordersExchange(){
+        return new TopicExchange(exchangeName);
+    }
+
+    @Bean
+    public Binding ordersBinding(Queue ordersQueue, TopicExchange ordersExchange){
+        return BindingBuilder.bind(ordersQueue)
+                .to(ordersExchange)
+                .with(routingOrdersKey);
+    }
 
     @Bean
     public Jackson2JsonMessageConverter jackson2JsonMessageConverter(){
